3767a2479b67de04185029549f6d218b0b851b62
Sairaj Aftab <sairajaftab6@gmail.com>
2024-11-03 18:44:23 +0600

    change

--- next-front/lib/route.js
+++ next-front/lib/route.js
@@ -1,4 +1,6 @@
 export const getDriver = async (id) => {
+  console.log(process.env.NEXT_PUBLIC_API ?? "No API URL");
+
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API}/driver/getsingle/${id}`
   );
