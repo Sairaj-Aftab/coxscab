529c21245cb4d608375c428ab9af0d9f9dc6e20e
Sairaj Aftab <sairajaftab6@gmail.com>
2024-11-15 03:14:59 +0600

    update here

--- next-front/app/driver/qrinfo/[id]/page.jsx
+++ next-front/app/driver/qrinfo/[id]/page.jsx
@@ -8,9 +8,9 @@ const DriverQrInfo = async ({ params }) => {
   return (
     <div>
       <div className="shadow-lg w-[95%] sm:w-[60%] md:w-[50%] lg:w-[40%] mx-auto p-5 rounded-md text-center">
-        <p>Coxscab ID : {driver?.driver.coxscabId}</p>
-        <p>{driver?.driver.name}</p>
-        <p>{driver?.driver.nameBn}</p>
+        <p>Coxscab ID : {driver?.driver?.coxscabId}</p>
+        <p>{driver?.driver?.name}</p>
+        <p>{driver?.driver?.nameBn}</p>
       </div>
     </div>
   );
