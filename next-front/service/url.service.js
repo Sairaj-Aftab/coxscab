1e82c8f4824f9fc3c66831aac63b88b059dfc2fe
Sairaj Aftab <sairajaftab6@gmail.com>
2024-12-04 05:55:14 +0600

    update here

--- next-front/service/url.service.js
+++ next-front/service/url.service.js
@@ -4,6 +4,9 @@ const ApiUrl = process.env.NEXT_PUBLIC_API;
 
 const axiosInstance = axios.create({
   baseURL: ApiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true,
 });
 
