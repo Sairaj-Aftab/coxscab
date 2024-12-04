74ac46ce95b83ea1006916bdac6cf2f9f4eb9596
Sairaj Aftab <sairajaftab6@gmail.com>
2024-12-04 21:40:54 +0600

    up

--- next-front/middleware.js
+++ next-front/middleware.js
@@ -2,14 +2,21 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const token = req.cookies.get("user_refresh_token");
+  const { pathname } = req.nextUrl;
+  console.log(token);
+  console.log(pathname);
 
-  if (token && req.nextUrl.pathname === "/auth-login") {
+  // Redirect authenticated users away from login
+  if (token && pathname === "/auth-login") {
     return NextResponse.redirect(new URL("/", req.url));
   }
-  if (!token && req.nextUrl.pathname === "/") {
+
+  // Redirect unauthenticated users to login
+  if (!token && (pathname === "/" || pathname === "/profile")) {
     return NextResponse.redirect(new URL("/auth-login", req.url));
   }
 
+  // Continue processing
   return NextResponse.next();
 }
 
